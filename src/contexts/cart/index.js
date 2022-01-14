ef74b0acbf7ef47b4ba9af62d38bc880ad2af0e9
deywid <deywid.felipe@gmail.com>
2022-01-13 22:11:16 -0300

    adding a remove option to cart

--- src/contexts/cart/index.js
+++ src/contexts/cart/index.js
@@ -1,21 +1,49 @@
-import { createContext } from "react";
-import { useState } from 'react'
+import { createContext, useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export const CartContext = createContext([]);
 
-
-export function CartProvider({children}) {
+export function CartProvider({ children }) {
 
   const [cart, setCart] = useState([]);
 
-  function handleAddCart(item){
-    setCart([... cart, item])
+  function handleAddCart(card) {
+
+    const newCard = {
+      ...card,
+      idCard: uuidv4()
+    }
+
+    setCart([...cart, newCard])
+    localStorage.setItem('cart', JSON.stringify([...cart, card]))
   }
+
+
+  function handleRemoveItemCart(idCard) {
+    const filteredCart = cart.filter(item => {
+      if (item.idCard === idCard) {
+        return false
+      } else {
+        return true
+      }
+    })
+    setCart(filteredCart)
+    localStorage.setItem('cart', JSON.stringify(filteredCart))
+  }
+
+  useEffect(() => {
+    function handleGetItensLocalStorage() {
+      const storedArray = JSON.parse(localStorage.getItem('cart'));
+      setCart(storedArray || []);
+    }
+    handleGetItensLocalStorage();
+  }, [])
 
   return (
     <CartContext.Provider value={{
       cart: cart,
-      addItem: handleAddCart
+      addItem: handleAddCart,
+      removeItem: handleRemoveItemCart
     }}>
       {children}
     </CartContext.Provider>
--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,22 +1,25 @@
 import React, { useContext } from "react";
-
+import { MdDelete } from "react-icons/md";
 import { CartContext } from '../../contexts/cart'
 
 function Cart() {
 
-  const { cart } = useContext(CartContext)
+  const { cart, removeItem } = useContext(CartContext)
 
   return (
     <>
       <table className="table-cart">
         <thead>
-          <th>Item</th>
-          <th>Nome</th>
-          <th>Preço</th>
+          <tr>
+            <th>Item</th>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Remover</th>
+          </tr>
         </thead>
         <tbody>
           {cart.map(item =>
-            <tr key={item.id}>
+            <tr key={item.idCard}>
               <td>
                 <img
                   src={item.card_images[0].image_url_small}
@@ -31,6 +34,13 @@ function Cart() {
                   { style: 'currency', currency: 'BRL' }
                 ).format(item.card_prices[0].cardmarket_price)
               }</td>
+              <td>
+                <MdDelete 
+                  size={30}
+                  color="#EC6D08"
+                  onClick={() => removeItem(item.idCard)}
+                />
+              </td>
             </tr>
           )}
         </tbody>
